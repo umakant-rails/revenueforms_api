3e223e906d23c04a0c2386c7be51021d194ec54c
umakant-rails <rajpootumakant@gmail.com>
2025-06-27 00:27:25 +0530

    update config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  scope(:path => '/api') do
+  scope(:path => '/api/rails') do
     devise_for :users, path: 'users', path_names: {
       sign_in: 'login',
       sign_out: 'logout',
